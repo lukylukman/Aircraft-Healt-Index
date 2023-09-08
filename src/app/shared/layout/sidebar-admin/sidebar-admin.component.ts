9036d7fb9b6ff43fcef31aba224ee9c786bf9327
surya.permana <surya.permana@opencloud.id>
2023-09-08 09:40:02 +0700

    [add] memisahkan tampilan antara user dan admin

---
+++ src/app/shared/layout/sidebar-admin/sidebar-admin.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnDestroy, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar-admin',
+  templateUrl: './sidebar-admin.component.html',
+  styleUrls: ['./sidebar-admin.component.css'],
+})
+export class SidebarAdminComponent implements OnInit, OnDestroy {
+  ngOnInit(): void {}
+  ngOnDestroy(): void {}
+}
