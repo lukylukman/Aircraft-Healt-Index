a7c2a847455ef1c78d35d17ea936ce1d18d09611
surya.permana <surya.permana@opencloud.id>
2023-09-11 10:54:45 +0700

    [update] memisahkan tampilan admin dan user

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { KeycloakService } from 'keycloak-angular';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(protected readonly keycloak: KeycloakService) {}
+  userRoles: string[] = [];
   title = 'ahi-angular';
-  login: boolean;
 
-  //Checking is Admin or User
-  isAdmin: boolean = true;
+  ngOnInit(): void {
+    this.userRoles = this.keycloak.getUserRoles();
+    console.log('Role =>', this.userRoles);
+  }
 }
