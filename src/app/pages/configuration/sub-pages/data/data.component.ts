dffaae316a9bed7b3519b06f56fda0d7507244b3
surya.permana <surya.permana@opencloud.id>
2023-09-15 13:56:29 +0700

    [update] menyiapkan proses upload

--- src/app/pages/configuration/sub-pages/data/data.component.ts
+++ src/app/pages/configuration/sub-pages/data/data.component.ts
@@ -1,10 +1,17 @@
-import { trigger, transition, style, animate, state } from '@angular/animations';
-import { Component } from '@angular/core';
-import { NgModel } from '@angular/forms';
+import {
+  trigger,
+  transition,
+  style,
+  animate,
+  state,
+} from '@angular/animations';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { FormBuilder, NgModel, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
+import { Subject } from 'rxjs';
 import { RouteHelperService } from 'src/app/core/services/route-helper.service';
 import { DashboardService } from 'src/app/pages/dashboard/dashboard.service';
-
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-data',
@@ -23,36 +30,52 @@ import { DashboardService } from 'src/app/pages/dashboard/dashboard.service';
         'true',
         style({
           transform: 'scale(1.10)', // Scale to 105%
-        }),
+        })
       ),
       state(
         'false',
         style({
           transform: 'scale(1)', // Default scale (100%)
-        }),
+        })
       ),
       transition('false => true', animate('200ms ease-out')),
       transition('true => false', animate('200ms ease-out')),
     ]),
   ],
 })
-export class DataComponent {
-
+export class DataComponent implements OnInit, OnDestroy {
   showUpdate: boolean = true;
   showCalculation: boolean = false;
+  AddDataForm;
 
   hilScore: number = 0;
   isUploading: boolean = false;
   uploadProgress: number = 0;
   customerName: string = '';
   selectedOption: string = ''; // Nilai default
-
+  selectedFile: File;
+  _onDestroy$: Subject<Boolean> = new Subject<Boolean>();
 
   constructor(
     private route: RouteHelperService, // private readonly unsubscribe$ = new Subject()
+    private formBuilder: FormBuilder,
     private readonly dashboardService: DashboardService,
-    private readonly store: Store,
-  ) {
+    private readonly store: Store
+  ) {}
+
+  ngOnDestroy(): void {
+    this._onDestroy$.next(true);
+    this._onDestroy$.unsubscribe();
+  }
+
+  ngOnInit(): void {
+    this.createForm();
+  }
+
+  createForm(): void {
+    this.AddDataForm = this.formBuilder.group({
+      upload_file: ['', [Validators.required]],
+    });
   }
 
   selectOption(option: string) {
@@ -64,32 +87,54 @@ export class DataComponent {
     }
   }
 
+  // public selectedFile: File;
+  onFileSelected(event: any): void {
+    if (event.target.files && event.target.files.length) {
+      this.selectedFile = event.target.files[0];
+      this.AddDataForm.patchValue({
+        upload_file: this.selectedFile.name,
+      });
+    }
+  }
+
   uploadFile(file: File, dataType: string): void {
     if (!file) {
       // Handle the case when no file is selected
-      alert('Please select a file');
+      // alert('Please select a file');
+      Swal.fire('Oops!', 'Please select a file!', 'warning');
       return;
     }
     if (dataType === 'Select config type') {
       // Handle the case when no data type is selected
-      alert('Please select a data type');
+      // alert('Please select a data type');
+      Swal.fire('Oops!', 'Please select a data type!', 'warning');
       return;
     }
     this.isUploading = true;
-    
-    this.dashboardService.updateDataConfiguration(file, dataType, this.customerName).subscribe(
-      (progress: number) => {
-        this.uploadProgress = progress;
-      },
-      (error) => {
-        console.error('Upload failed:', error);
-      },
-      () => {
-        this.isUploading = false;
-        this.uploadProgress = 0;
-        console.log('upload success');
-      }
-    );
-  }
 
+    this.dashboardService
+      .updateDataConfiguration(file, dataType, this.customerName)
+      .subscribe(
+        (progress: number) => {
+          this.uploadProgress = progress;
+        },
+        (error) => {
+          Swal.fire('Oops!', 'Upload failed!', 'warning');
+          // console.error('Upload failed:', error);
+        },
+        () => {
+          this.isUploading = false;
+          this.uploadProgress = 0;
+
+          // TODO
+          // Kirim formData lewat component aja
+          const formData = new FormData();
+          formData.append('file', this.selectedFile);
+          formData.append('customerName', this.customerName);
+
+          Swal.fire('Yeaay!', 'Upload success!', 'success');
+          // console.log('upload success');
+        }
+      );
+  }
 }
