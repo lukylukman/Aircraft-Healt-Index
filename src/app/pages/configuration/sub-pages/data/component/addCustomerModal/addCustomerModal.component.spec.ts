4a2c9cbd70716304829d89bc9c9dc26e9fa9a189
Lucky Lukman Hakim <luky.hakim@opencloud.id>
2023-10-01 10:20:29 +0700

    [fix] name folder

--- src/app/pages/configuration/sub-pages/data/component/addCustomerModal/addCustomerModal.component.spec.ts
+++ src/app/pages/configuration/sub-pages/data/component/addCustomerModal/addCustomerModal.component.spec.ts
@@ -3,7 +3,7 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { By } from '@angular/platform-browser';
 import { DebugElement } from '@angular/core';
 
-import { AddNewCustomerModalComponent } from './addNewUserModal.component';
+import { AddNewCustomerModalComponent } from './addCustomerModal.component';
 
 describe('AddNewUserModalComponent', () => {
   let component: AddNewCustomerModalComponent;
